77b4efc3a123c909f3018bc04e7b1ed890e6025e
gashpen <constantinent@mail.ru>
2023-04-08 13:55:53 +0300

    homework

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,13 +1,22 @@
-let checkboxes = document.querySelectorAll('.interest__check');
-let checked = document.querySelectorAll(".interests_active");
+const checked = document.querySelectorAll(".interests_active");
+const interest = document.querySelectorAll(".interest__check");
+const selectedInterests = document.querySelectorAll( '.interest__check:checked' );
 
- 
-checkboxes = Array.from(checkboxes)
-
-for(let i = 0; i < checkboxes.length;i++){
-    checkboxes[i].addEventListener('click',function(){
-        if(checkboxes[i].checked){
-            
+Array.from(interest).forEach(element=>{
+    element.addEventListener("click",()=>{
+        if(interest[0].checked === true){
+            interest[1].checked = true;
+            interest[2].checked = true;
+        } else {
+            interest[1].checked = false;
+            interest[2].checked = false;
+        }
+        if(interest[3].checked === true){
+            interest[4].checked = true;
+            interest[5].checked = true;
+        } else {
+            interest[4].checked = false;
+            interest[5].checked = false;  
         }
     })
-}
+})
